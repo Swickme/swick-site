ee75dbdf0889a9e059e6e8d0c79c9421a4591306
root <root@Sarvesh-Pc.localdomain>
2022-01-27 18:48:26 +0530

    yml file + permisons

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <iframe src="https://example-0123.s3.us-east-2.amazonaws.com/samplevideo.mp4" name="iframe_a" height="800px" width="100%" title="Iframe Example"></iframe>
+    </div>
+  );
+}
+
+export default App;
